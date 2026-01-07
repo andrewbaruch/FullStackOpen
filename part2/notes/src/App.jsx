c6d17ba554fa700dfb467e04e50eda0d5bc398b0
Andrew Baruch <andrewbaruch4@gmail.com>
2026-01-07 19:09:48 +0800

    exercise 2.11

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,21 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
 
-  const addNote = (event) => {    
+  useEffect(() => { 
+    console.log('effect')    
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {console.log('promise fulfilled')
+      setNotes(response.data) }) 
+  }, [])
+  
+  console.log('render', notes.length, 'notes')
+
+  const addNote = (event) => {
     event.preventDefault()
-    console.log('button clicked', event.target) 
+    console.log('button clicked', event.target)
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
       id: String(notes.length + 1),
     }
     setNotes(notes.concat(noteObject))
-    setNewNote('') 
+    setNewNote('')
   }
 
   const handleNoteChange = (event) => {
@@ -23,7 +34,7 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
 
-  const notesToShow = showAll    ? notes    : notes.filter(note => note.important === true)
+  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
 
   return (
     <div>
@@ -38,9 +49,9 @@ const App = (props) => {
       </ul>
       <form onSubmit={() => addNote}>
         <input value={newNote}
-          onChange={handleNoteChange}/>
-          <button type="submit">save</button>
-        </form>
+          onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
-import { use } from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Contacts = ({contacts}) => {
   return (
@@ -66,18 +66,22 @@ const Filter = ({filter, setFilter}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [filter, setFilter] = useState('')
   const filterFunction = (entry) => {
     // console.log(entry.name.toLowerCase(), filter.toLowerCase(), entry.name.toLowerCase().includes(filter.toLowerCase()))
     return(entry.name.toLowerCase().includes(filter.toLowerCase()))
   }
+
+  useEffect(() => {
+    console.log('Effect Called')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('data pulled')
+        setPersons(response.data)
+      })
+  }, [])
 
   let filteredMap = persons.filter(entry => filterFunction(entry))
   // console.log(filteredMap)
